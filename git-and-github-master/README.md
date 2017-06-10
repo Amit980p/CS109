104e3a0753fb87576e3975956c96b4480fc7050b
AMIT <amit980p@gmail.com>
2017-06-10 19:26:31 +0530

    This is the first time i am using git

---
+++ git-and-github-master/README.md
@@ -0,0 +1,11 @@
+# AC297r Tools Tutorial
+
+An introduction to git:
+
+ - Git Basics
+ - Common Commands
+ - Common Tasks
+ - Hosting Git Repositories on Github
+ - Collaboration
+ - Github Pages
+ - nbviewer
---
+++ teachers_pet-master/lib/teachers_pet/commands/push_files.rb
@@ -0,0 +1,16 @@
+module TeachersPet
+  class Cli
+    option :organization, required: true
+    option :repository, required: true
+    option :branch, banner: 'BRANCH', default: 'master', desc: "The branch to push to."
+
+    students_option
+    common_options
+    option :ssh, banner: 'HOST', default: TeachersPet::Configuration.sshEndpoint
+
+    desc 'push_files', "Run this command from a local Git repository to push the files up to the specified repository for each student. It will add a remote that is the name of each student team to your repository."
+    def push_files
+      TeachersPet::Actions::PushFiles.new(options).run
+    end
+  end
+end
